64e976c284a7d235e8e08c4f89a9cbf25577efdd
Andrii Serdiuk <bihhi2@gmail.com>
2024-03-29 17:05:20 +0200

    last-commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function slugify(title) {
+
+    let lowercaseTitle = title.toLowerCase();
+    let words = lowercaseTitle.split(' ');
+    let slug = words.join('-');
+    return slug;
+    
+}
+
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
